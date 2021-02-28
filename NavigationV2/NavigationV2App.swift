d4b11c7ec51c8afcf1f8decf93a4d57902a87366
Ben Stone <benjamin.h.stone@gmail.com>
2021-02-27 20:42:14 -0500

    Initial Commit

---
+++ NavigationV2/NavigationV2App.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationV2App.swift
+//  NavigationV2
+//
+//  Created by Ben Stone on 2/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
