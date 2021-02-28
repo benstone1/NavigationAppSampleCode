426861ea07101576c95d9a997f9764ea61d0eb01
Ben Stone <benjamin.h.stone@gmail.com>
2021-02-27 21:57:53 -0500

    add new detail view

--- NavigationV2/ContentView.swift
+++ NavigationV2/ContentView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text("Items in Stock")
+                    .font(.title)
+                    .padding()
+                Spacer()
+                NavigationLink(
+                    destination: ItemDetailView(itemName: "Shrimp Chips"),
+                    label: {
+                        Text("Shrimp Chips")
+                    })
+                Spacer()
+            }
+            .navigationTitle(Text("Ligaya's Store"))
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
